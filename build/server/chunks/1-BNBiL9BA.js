40614a0be37299c681f1bdab5e6f1548acd0ccb9
jmssalas <hola@jmssalas.com>
2025-01-12 17:34:13 +0100

    Remove build folder

--- build/server/chunks/1-BNBiL9BA.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-B9IN3zFW.js')).default;
-const imports = ["_app/immutable/nodes/1.Ey6YnjuU.js","_app/immutable/chunks/disclose-version.CtQfRNkA.js","_app/immutable/chunks/runtime.B5iW8Zo5.js","_app/immutable/chunks/legacy.CNmd39ch.js","_app/immutable/chunks/store.D6VYYOxz.js","_app/immutable/chunks/utils.DIs2teQl.js","_app/immutable/chunks/entry.CT896-Jm.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-BNBiL9BA.js.map
